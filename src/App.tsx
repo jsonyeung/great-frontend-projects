86726f18353119521d57a09555618cec7075863c
Jason Yeung <jsonyeung@outlook.com>
2024-06-27 14:11:28 -0700

    Setup Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
 
+/*
 function App() {
   const [count, setCount] = useState(0)
 
@@ -31,5 +37,6 @@ function App() {
     </>
   )
 }
+*/
 
 export default App
