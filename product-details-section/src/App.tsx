a570d7520cb7bfca1cf45f46e00343e1793c8c11
Jason Yeung <jsonyeung@outlook.com>
2024-07-04 17:11:27 -0700

    Add boilerplate

---
+++ product-details-section/src/App.tsx
@@ -0,0 +1,33 @@
+import "@fontsource-variable/noto-sans";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#F9FAFB] to-[#D2D6DB] p-4">
+      <div className="bg-white shadow-md rounded-l py-24 px-4">
+        {/* Gallery */}
+
+        <h1>Voyager Hoodie</h1>
+
+        The Voyager Hoodie is for the explorer at heart. Its durable fabric and roomy pockets are perfect for those who are always searching for the next adventure.
+
+        {/* Price */}
+
+        {/* Label */}
+
+        {/* Color Selector */}
+
+        {/* Size Selector */}
+
+        {/* Quantity Selector */}
+
+        {/* Button */}
+
+        {/* Accordian */}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
