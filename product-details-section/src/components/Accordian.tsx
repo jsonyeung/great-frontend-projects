3286fb4618a6a8a8b3cd9680f8ca6cb79f5f478d
Jason Yeung <jsonyeung@outlook.com>
2024-07-15 15:43:21 -0700

    Add placeholder data and close toggle symbol

--- product-details-section/src/components/Accordian.tsx
+++ product-details-section/src/components/Accordian.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { RiAddCircleLine } from "react-icons/ri";
+import { RiAddCircleLine, RiIndeterminateCircleLine } from "react-icons/ri";
 
 function Accordion() {
   const [isOpened, setIsOpened] = useState(new Set());
@@ -11,15 +11,37 @@ function Accordion() {
       content: (
         <ul className="list-inside list-disc text-gray-600">
           <li>Designed for comfort and durability.</li>
+          <li>Soft, breathable fabric ideal for travel and adventure.</li>
+          <li>Lace front procket and adjustable hood.</li>
+          <li>Minimalist design pairs well with any style.</li>
+          <li>Made with eco-conscious materials.</li>
         </ul>
       ),
     },
     {
       id: 1,
-      title: "Specification",
+      title: "Fabric & Care",
       content: (
         <ul className="list-inside list-disc text-gray-600">
-          <li>Designed for comfort and durability.</li>
+          <li>Machine wash cold on a gentle cycle.</li>
+          <li>Tumble dry on low heat with tennis balls to maintain loft.</li>
+          <li>Do not iron; steam if necessary.</li>
+          <li>Can be dry cleaned if preferred</li>
+        </ul>
+      ),
+    },
+    {
+      id: 2,
+      title: "Shipping",
+      content: (
+        <ul className="list-inside list-disc pr-10 text-gray-600">
+          <li>
+            Free standard shipping on all orders - no minimum spend required.
+          </li>
+          <li>Expedited shipping available at an additional cost.</li>
+          <li>
+            Packaged in biodegradable materials to reduce environmental impact
+          </li>
         </ul>
       ),
     },
@@ -44,14 +66,19 @@ function Accordion() {
           <div className="border-b border-neutral-300 last:border-none">
             <div
               onClick={() => handleAccordionItemToggle(itemData)}
-              className="cursor-pointer flex items-center justify-between py-6"
+              className="cursor-pointer flex items-center justify-between py-7"
             >
               <h3 className="text-lg font-medium">{itemData.title}</h3>
-              <RiAddCircleLine size={"1.5rem"} />
+
+              {isOpened.has(itemData.id) ? (
+                <RiIndeterminateCircleLine size={"1.5rem"} />
+              ) : (
+                <RiAddCircleLine size={"1.5rem"} />
+              )}
             </div>
 
             <div
-              className={`mt-2 overflow-hidden ${isOpened.has(itemData.id) ? "h-auto" : "h-0"}`}
+              className={`overflow-hidden ${isOpened.has(itemData.id) ? "h-auto -mt-5 mb-7" : "h-0"}`}
             >
               {itemData.content}
             </div>
